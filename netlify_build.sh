56612e9ab96564478b93e6b8d99291d28081dd79
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-07-21 11:46:35 -0400

    source bashrc

--- netlify_build.sh
+++ netlify_build.sh
@@ -6,10 +6,12 @@ echo "Installing mamba-forge"
 curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
 yes yes| bash Mambaforge-$(uname)-$(uname -m).sh
 
+source /opt/buildhome/.bashrc;
 
 echo "Installing snakemake"
 mamba install -c conda-forge -c bioconda snakemake r-tidyverse r-blogdown r-xfun r-tufte r-cowplot;
 
+source /opt/buildhome/.bashrc;
 conda activate base;
 
 echo "Running snakemake"
