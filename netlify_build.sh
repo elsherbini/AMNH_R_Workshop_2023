352a01f9141f78371b12dcd0d42e5303ad26deb6
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-14 19:09:52 -0400

    oof downgrading things really sucks

--- netlify_build.sh
+++ netlify_build.sh
@@ -11,7 +11,7 @@ yes yes| bash Mambaforge-$(uname)-$(uname -m).sh
 source /opt/buildhome/.bashrc;
 
 echo "Installing snakemake"
-mamba install -c conda-forge -c bioconda r-remotes snakemake r-tidyverse r-knitr=1.30 r-xfun=0.19 r-tufte r-cowplot;
+mamba install -c conda-forge -c bioconda r-remotes snakemake r-tidyverse r-knitr=1.30 r-xfun=0.29 r-tufte r-cowplot;
  Rscript -e 'remotes::install_github("rstudio/blogdown@2637c1cdfb997f0ff113cfa2225533a3d88f87c1", upgrade="never")';
 
 source /opt/buildhome/.bashrc;
