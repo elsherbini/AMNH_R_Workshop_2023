2fa36b4b08f264e9cd8256f6a6d682f4bcd8beef
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-14 17:56:41 -0400

    remotes was upgrading all my downgrades

--- netlify_build.sh
+++ netlify_build.sh
@@ -12,7 +12,7 @@ source /opt/buildhome/.bashrc;
 
 echo "Installing snakemake"
 mamba install -c conda-forge -c bioconda r-remotes snakemake r-tidyverse r-knitr=1.30 r-blogdown=0.21 r-xfun=0.19 r-tufte r-cowplot;
- Rscript -e 'remotes::install_github("rstudio/blogdown@2637c1cdfb997f0ff113cfa2225533a3d88f87c1")';
+ Rscript -e 'remotes::install_github("rstudio/blogdown@2637c1cdfb997f0ff113cfa2225533a3d88f87c1", upgrade="never")';
 
 source /opt/buildhome/.bashrc;
 conda activate base;
