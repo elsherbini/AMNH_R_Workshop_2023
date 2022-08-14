092206cb4bab5201568bb8c2ea27442988ae8d54
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-14 17:39:09 -0400

    downgrade tidyverse too

--- netlify_build.sh
+++ netlify_build.sh
@@ -11,7 +11,7 @@ yes yes| bash Mambaforge-$(uname)-$(uname -m).sh
 source /opt/buildhome/.bashrc;
 
 echo "Installing snakemake"
-mamba install -c conda-forge -c bioconda r-remotes snakemake r-tidyverse r-blogdown=0.21 r-xfun r-tufte r-cowplot;
+mamba install -c conda-forge -c bioconda r-remotes snakemake r-tidyverse=1.3.0 r-blogdown=0.21 r-xfun r-tufte r-cowplot;
  Rscript -e 'remotes::install_github("rstudio/blogdown@2637c1cdfb997f0ff113cfa2225533a3d88f87c1")';
 
 source /opt/buildhome/.bashrc;
