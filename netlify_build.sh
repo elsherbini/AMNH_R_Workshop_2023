4100947b6ec056e6e181f773fbac24cb95f62f6f
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-15 12:02:17 -0400

    learned way too much about the render.R history in the blogdown repo. I think this is the right version.

--- netlify_build.sh
+++ netlify_build.sh
@@ -16,7 +16,7 @@ mamba install -c conda-forge -c bioconda snakemake r-xfun r-bookdown=0.20 r-remo
 source /opt/buildhome/.bashrc;
 conda activate /opt/build/repo/yes;
 
- Rscript -e 'remotes::install_github("rstudio/blogdown@89e9efcf1b59cb85f6f1ad114f4f225b837a410f", upgrade="never")';
+ Rscript -e 'remotes::install_github("rstudio/blogdown@0e2e33f71adadf3d85aac4fa0fb23584721d4b75", upgrade="never")';
 
 
 echo "Running snakemake"
