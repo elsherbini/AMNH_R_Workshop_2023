0b637d344058330f72fa62e2cd4ba8855e9f1d07
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-15 13:09:38 -0400

    print debugging

--- netlify_build.sh
+++ netlify_build.sh
@@ -13,8 +13,10 @@ source /opt/buildhome/.bashrc;
 echo "Installing snakemake"
 mamba install -c conda-forge -c bioconda snakemake r-xfun r-bookdown=0.20 r-remotes r-tidyverse r-tufte r-cowplot;
 
+echo "activating conda"
 source /opt/buildhome/.bashrc;
 conda activate /opt/build/repo/yes;
+echo $PATH
 
  Rscript -e 'remotes::install_github("rstudio/blogdown@0e2e33f71adadf3d85aac4fa0fb23584721d4b75", upgrade="never")';
 
