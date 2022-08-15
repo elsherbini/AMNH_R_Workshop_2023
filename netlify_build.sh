81439d4345e5de37a61686b3ebd1021c885c8b26
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-15 18:52:31 -0400

    no way this works x3

--- netlify_build.sh
+++ netlify_build.sh
@@ -11,15 +11,14 @@ yes yes| bash Mambaforge-$(uname)-$(uname -m).sh
 source /opt/buildhome/.bashrc;
 
 echo "Installing snakemake"
-mamba install -c conda-forge -c bioconda snakemake r-blogdown=0.21 r-remotes r-tufte=0.9;
+mamba install -c conda-forge -c bioconda snakemake r-blogdown=0.21 r-xfun=0.19 r-bookdown=0.26 r-tidyverse r-cowplot r-remotes r-tufte=0.9;
 
 echo "activating conda"
 source /opt/buildhome/.bashrc;
 conda activate /opt/build/repo/yes;
 echo $PATH
 
-Rscript -e 'library(xfun); sessionInfo(); xfun::dir_exists;'
-Rscript -e 'library(xfun); dir_exists = function(x) file_test("-d", x); export(dir_exists); remotes::install_github("rstudio/blogdown@0e2e33f71adadf3d85aac4fa0fb23584721d4b75")';
+Rscript -e 'remotes::install_github("rstudio/blogdown@0e2e33f71adadf3d85aac4fa0fb23584721d4b75")';
 
 
 echo "Running snakemake"
