3f55a1361ad9fed548e8086731e24b6f60c9a0eb
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-15 14:06:38 -0400

    further descent into madness

--- netlify_build.sh
+++ netlify_build.sh
@@ -18,7 +18,7 @@ source /opt/buildhome/.bashrc;
 conda activate /opt/build/repo/yes;
 echo $PATH
 
- Rscript -e 'remotes::install_github("rstudio/blogdown@0e2e33f71adadf3d85aac4fa0fb23584721d4b75", upgrade="never", force=TRUE)';
+ Rscript -e 'remotes::install_github("rstudio/blogdown@0e2e33f71adadf3d85aac4fa0fb23584721d4b75")';
 
 
 echo "Running snakemake"
