a69544f1acb6ca1a1a0b573942dd112a975bfbf0
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-15 09:41:37 -0400

    using yes also makes the default conda directory called yes

--- netlify_build.sh
+++ netlify_build.sh
@@ -11,11 +11,13 @@ yes yes| bash Mambaforge-$(uname)-$(uname -m).sh
 source /opt/buildhome/.bashrc;
 
 echo "Installing snakemake"
-mamba install -c conda-forge -c bioconda snakemake r-tidyverse r-knitr=1.30 r-tufte r-cowplot;
- Rscript -e 'remotes::install_github("rstudio/blogdown@2637c1cdfb997f0ff113cfa2225533a3d88f87c1", upgrade="never")';
+mamba install -c conda-forge -c bioconda snakemake r-remotes r-tidyverse r-knitr=1.30 r-tufte r-cowplot;
 
 source /opt/buildhome/.bashrc;
-conda activate base;
+conda activate ~/yes;
+
+ Rscript -e 'remotes::install_github("rstudio/blogdown@2637c1cdfb997f0ff113cfa2225533a3d88f87c1", upgrade="never")';
+
 
 echo "Running snakemake"
 snakemake -j 1;
