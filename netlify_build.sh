ee8b712509044b61b05802fad4cc3b1b8976b187
Joseph Elsherbini <joseph.elsherbini@gmail.com>
2022-08-16 05:43:36 -0400

    more madness

--- netlify_build.sh
+++ netlify_build.sh
@@ -11,12 +11,14 @@ yes yes| bash Mambaforge-$(uname)-$(uname -m).sh
 source /opt/buildhome/.bashrc;
 
 echo "Installing snakemake"
-mamba install -c conda-forge -c bioconda snakemake r-blogdown=0.21 r-knitr=1.31 r-xfun r-bookdown=0.17 r-tidyverse r-cowplot r-remotes r-tufte=0.8;
+mamba install -c conda-forge -c bioconda snakemake r-blogdown=0.21 r-knitr=1.30 r-xfun r-bookdown=0.17 r-tidyverse r-cowplot r-remotes r-tufte=0.8;
 
 echo "activating conda"
 source /opt/buildhome/.bashrc;
 conda activate /opt/build/repo/yes;
 echo $PATH
+
+Rscript -e 'remotes::install_github("yihui/knitr@54577dd5cac5cee9887aa44235cb37611be92411", upgrade="never", force=TRUE)';
 
 Rscript -e 'remotes::install_github("rstudio/blogdown@0e2e33f71adadf3d85aac4fa0fb23584721d4b75", upgrade="never", force=TRUE)';
 
